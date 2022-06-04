1f72ce93c17ccd4e687dbab08f603c4065f4dcfd
keikosmile <69710419+keikosmile@users.noreply.github.com>
2022-06-05 02:27:28 +0900

    load_pathのパスを修正。modelとviewのディレクトリ作成。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # すべてのリクエストの冒頭でロケールを設定する
   before_action :set_locale
 
   private
@@ -9,6 +10,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  # すべてのURL生成用のグローバルなデフォルトパラメータを設定する
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,5 @@
 # I18nライブラリに訳文の探索場所を指示する
-I18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
 #アプリケーションで有効とする言語を指定する（英語と日本語）
 I18n.available_locales = [:en, :ja]
