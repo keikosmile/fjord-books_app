ec8c45d9549668c0a405267bec620c5b86e6ad65
keikosmile <69710419+keikosmile@users.noreply.github.com>
2022-06-06 21:51:28 +0900

    before_actionからaround_actionに変更。I18n.localeからI18n.with_localeに変更。言語を切り替えるリンクを追加。コメントを削除。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # すべてのリクエストの冒頭でロケールを設定する
-  before_action :set_locale
+  around_action :switch_locale
 
   private
 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 
-  # すべてのURL生成用のグローバルなデフォルトパラメータを設定する
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,8 +1,5 @@
-# I18nライブラリに訳文の探索場所を指示する
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
-#アプリケーションで有効とする言語を指定する（英語と日本語）
 I18n.available_locales = [:en, :ja]
 
-# デフォルトの言語を指定する（日本語）
 I18n.default_locale = :ja
